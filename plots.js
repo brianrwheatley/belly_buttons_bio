05129aa207e0869ff1299d9938b71ed111c5fbf4
brianrwheatley <96029155+brianrwheatley@users.noreply.github.com>
2022-06-14 18:53:17 -0700

    new index

--- plots.js
+++ plots.js
@@ -1,19 +1,37 @@
-function init() {
-  var selector = d3.select("#selDataset");
+// Sort the data array using the greekSearchResults value
+data.sort(function(a, b) {
+  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+});
 
-  d3.json("samples.json").then((data) => {
-    console.log(data);
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-})}
+// Slice the first 10 objects for plotting
+data = data.slice(0, 10);
 
-init();
+// Reverse the array due to Plotly's defaults
+data = data.reverse();
 
-function optionChanged(newSample) {
-  console.log(newSample);
-}
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
+
+// data
+var data = [trace1];
+
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
